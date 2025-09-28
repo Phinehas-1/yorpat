8ac8bcd57b707764a524da9978a51df449adf401
phinehas <phinehasenakireru@gmail.com>
2025-09-28 10:37:44 +0100

    define single postgres testcontainer for all tests

--- src/test/java/com/bigdecimal/yorpat/TestcontainersConfiguration.java
+++ src/test/java/com/bigdecimal/yorpat/TestcontainersConfiguration.java
@@ -4,15 +4,19 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
+	@SuppressWarnings("resource")
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+		return new PostgreSQLContainer<>("postgres:16.2-alpine")
+				.withDatabaseName("testdb")
+				.withUsername("test")
+				.withPassword("test")
+				.withInitScript("db/data.sql");
 	}
 
 }
