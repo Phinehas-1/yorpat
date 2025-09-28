e8bb5e364a5d0ce1c431262ca976c447b6321819
phinehas <phinehasenakireru@gmail.com>
2025-09-28 10:40:56 +0100

    replace AdminUser entity with User entity, more generic

---
+++ src/main/java/com/bigdecimal/yorpat/User.java
@@ -0,0 +1,39 @@
+package com.bigdecimal.yorpat;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid")
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private boolean userIsLoggedIn = false;
+}
